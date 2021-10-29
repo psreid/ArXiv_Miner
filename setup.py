4345bc219611f891b70e2313bb6c9c8d018ced4c
parkerreid <psreid@sfu.ca>
2021-10-29 16:28:27 -0700

    requirements

--- setup.py
+++ setup.py
@@ -3,7 +3,11 @@ from setuptools import setup
 setup(
     name='ArXiv_Miner',
     version='0.1',
-    packages=['pandas','pdftotext'],
+    packages=[''],
+    install_requires=[
+        'pandas~ = 1.3.2'
+        'pdftotext ~= 2.2.0']
+    ,
     url='',
     license='',
     author='parkerreid',
